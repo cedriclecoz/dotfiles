8fc5e3fa0499d823f8ee3c86d13170976c5f5499
Cedric Le Coz <cedric.le.coz@accenture.com>
2016-07-04 16:37:26 +0100

    Add prompt to clean.sh script

--- clean.sh
+++ clean.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+echo "This script is going to remove:"
+echo " ~/.bashrc"
+echo " ~/.vim"
+echo " ~/.vimrc"
+echo " ~/.inputrc"
+echo " vim/vim/bundle"
+if [ -L ~/dotfiles ]; then
+    echo " ~/dotfiles"
+fi
+echo "Terminal might be unstable following this removal."
+read -n1 -r -p "Press a key to continue... Ctrl-C to cancel" key
+
 rm ~/.bashrc ~/.vim ~/.vimrc ~/.inputrc 
 rm -rf vim/vim/bundle
 
