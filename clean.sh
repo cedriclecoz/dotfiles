b5703eebc4035cdf60d32c28f9b560830374fedb
Cedric Le Coz <cedric.le.coz@accenture.com>
2016-07-06 17:25:58 +0100

    remove some file if symbolic link only

--- clean.sh
+++ clean.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 
-echo "This script is going to remove:"
+echo "This script is going to remove the following symbolic links:"
 echo " ~/.bashrc"
 echo " ~/.vim"
 echo " ~/.vimrc"
 echo " ~/.inputrc"
-echo " vim/vim/bundle"
-if [ -L ~/dotfiles ]; then
-    echo " ~/dotfiles"
-fi
+echo " ~/.gitignore"
+echo " ~/dotfiles"
+echo " and the folder vim/vim/bundle"
+
 echo "Terminal might be unstable following this removal."
 read -n1 -r -p "Press a key to continue... Ctrl-C to cancel" key
+listToRemove=" ~/.bashrc ~/.vim ~/.vimrc ~/.inputrc ~/dotfile/ "
 
-rm ~/.bashrc ~/.vim ~/.vimrc ~/.inputrc 
+for file in ${listToRemove}; do
+    if [ -L ~/dotfiles ]; then
+        echo ${file} is symbolic link, remove
+    else
+        echo ${file} is not symbolic link, skip
+    fi
+done
+
+echo "remove vim/vim/bundle"
 rm -rf vim/vim/bundle
-
-if [ -L ~/dotfiles ]; then
-    rm ~/dotfiles
-fi
-
 
